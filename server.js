342baa0836e84e0613f581f5a141d494ae188be5
Raj Kumar <36179005+rajbasnet0007@users.noreply.github.com>
2024-04-23 14:30:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Node Home App - version-1!!</h1> \n');
+  res.send('<h1 style="color:green;">Node Home App - version-1!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
